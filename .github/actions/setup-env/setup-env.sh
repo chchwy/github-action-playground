31f872da854efb11a2c96aeccaad72f0ce275cb6
Matt Chang <chchwy@gmail.com>
2024-06-18 12:13:00 +0800

    Test env

--- .github/actions/setup-env/setup-env.sh
+++ .github/actions/setup-env/setup-env.sh
@@ -1,7 +1,7 @@
 
 # set the environment variable BRANCH_NAME to the current branch name
-echo "github.ref is " ${{github.ref}}
-echo "GITHUB_REF_NAME=${GITHUB_REF##*/}" >> "${GITHUB_ENV}"
+echo "github.ref is ${{github.ref}}"
+echo "GITHUB_REF_NAME=${GITHUB_REF_NAME}" >> "${GITHUB_ENV}"
 echo "GITHUB_REF_NAME is ${GITHUB_REF_NAME}"
 
 echo "BRANCH_NAME=${GITHUB_REF_NAME}" >> "${GITHUB_ENV}"
